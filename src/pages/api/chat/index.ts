836a052b03bfe04fae86223bcd0b57fca52e6226
CWIN77 <cwstbp0426@gmail.com>
2022-11-24 23:52:33 +0900

    방 생성 완료

--- src/pages/api/chat/index.ts
+++ src/pages/api/chat/index.ts
@@ -3,8 +3,8 @@ import { NextApiResponseServerIO } from "../../../types/chat";
 
 export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
   if (req.method === "POST") {
-    const message = req.body;
-    res.socket.server.io.emit("message", message);
+    const { message, roomName } = req.body;
+    res.socket.server.io.emit(roomName, message);
 
     res.status(201).json(message);
   }
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect, useCallback } from "react";
-import { connect } from "socket.io-client";
+import { connect, Socket } from "socket.io-client";
 import axios from "axios";
 
 interface IMessage {
@@ -12,49 +12,49 @@ export default function Chat() {
   const [username, setUsername] = useState<string>("");
   const [roomName, setRoomName] = useState<string>("");
   const [connected, setConnected] = useState<boolean>(false);
-  const socket = connect("http://localhost:8080", {
-    path: "/api/chat/socketio",
-  });
+  const [socket, setSocket] = useState<Socket>();
 
   useEffect((): any => {
+    const socket = connect("http://localhost:8080", {
+      path: "/api/chat/socketio",
+    });
+    setSocket(socket);
+
     socket.on("connect", () => {
       console.log("SOCKET CONNECTED!", socket.id);
       setConnected(true);
     });
 
-    socket.on("message", (message: IMessage) => {
-      console.log(message);
-    });
-
     if (socket) return () => socket.disconnect();
   }, []);
 
-  // const enterRoom = () => {
-  //   socket.emit("enter_room", roomName); 
-  // }
+  const enterRoom = () => {
+    socket?.on(roomName, (message: IMessage) => {
+      console.log(message);
+    });
+  }
 
-  const submitSendMessage = async (
-    event: React.FormEvent<HTMLButtonElement>
-  ) => {
+  const submitSendMessage = async (event: React.FormEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    if (sendMessage) {
+    if (sendMessage && roomName !== "") {
       const message: IMessage = {
         user: username,
         message: sendMessage,
       };
 
-      await axios.post("/api/chat", message);
+      await axios.post("/api/chat", { message, roomName });
       setSendMessage("");
     }
   };
 
   return (
     <div>
-      {/* <input placeholder="방이름" type={"text"} onChange={(e) => { setRoomName(e.target.value) }} /> */}
-      {/* <button onClick={enterRoom}>방참가</button> */}
-      <input placeholder="이름" type={"text"} onChange={(e) => { setSendMessage(e.target.value) }} />
-      <input placeholder="텍스트" type={"text"} onChange={(e) => { setUsername(e.target.value) }} />
+      <input value={roomName} placeholder="방이름" type={"text"} onChange={(e) => { setRoomName(e.target.value) }} />
+      <button onClick={enterRoom}>방참가</button>
+      <input value={sendMessage} placeholder="이름" type={"text"} onChange={(e) => { setSendMessage(e.target.value) }} />
+      <input value={username} placeholder="텍스트" type={"text"} onChange={(e) => { setUsername(e.target.value) }} />
       <button onClick={submitSendMessage}>전송</button>
+      {/* <button onClick={joinRoom}>전송</button> */}
     </div>
   );
 };
