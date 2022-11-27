7ebda8cee2cffde2c05b854e0578ac8e0054b9dd
CWIN77 <cwstbp0426@gmail.com>
2022-11-27 20:14:06 +0900

    webrtc 생성

--- src/pages/api/chat/index.ts
+++ src/pages/api/chat/index.ts
@@ -3,9 +3,17 @@ import { NextApiResponseServerIO } from "../../../types/chat";
 
 export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
   if (req.method === "POST") {
-    const { message, roomName } = req.body;
-    res.socket.server.io.emit(roomName, message);
+    if (req.body.message) {
+      const { message, roomName } = req.body;
+      res.socket.server.io.emit(roomName, message);
+    } else if (req.body.offer) {
+      const { offer, roomName } = req.body;
+      res.socket.server.io.emit(roomName + "offer", offer);
+    } else if (req.body.answer) {
+      const { answer, roomName } = req.body;
+      res.socket.server.io.emit(roomName + "answer", answer);
+    }
 
-    res.status(201).json(message);
+    res.status(201).json("굿");
   }
 };
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -13,9 +13,25 @@ export default function Chat() {
   const [roomName, setRoomName] = useState<string>("");
   const [connected, setConnected] = useState<boolean>(false);
   const [socket, setSocket] = useState<Socket>();
+  // let peerConnection: RTCPeerConnection | null = null;
+  // const [peerConnection, setPeerConnection] = useState<RTCPeerConnection | null>(null);
+
+  const getUserMedia = async () => {
+    const myStream = await navigator.mediaDevices.getUserMedia(
+      {
+        audio: true,
+        video: true
+      }
+    );
+    const video: any = document.getElementById("video");
+    video.srcObject = myStream;
+    return myStream;
+  }
 
   useEffect((): any => {
-    const socket = connect("http://localhost:8080", {
+    getUserMedia();
+
+    const socket = connect("http://localhost:4000", {
       path: "/api/chat/socketio",
     });
     setSocket(socket);
@@ -26,11 +42,47 @@ export default function Chat() {
     });
 
     if (socket) return () => socket.disconnect();
-  }, []);
+  }, [])
 
-  const enterRoom = () => {
+
+  const handleIce = (data: any) => {
+    console.log("candidate를 받음");
+    console.log(data);
+  }
+
+  const enterRoom = async () => {
+    const peerConnection = new RTCPeerConnection();
+    const myStream = await getUserMedia()
+    myStream.getTracks().forEach(track => peerConnection.addTrack(track, myStream))
+    peerConnection.addEventListener('icecandidate', (data) => {
+      console.log(data);
+    });
+
+    await axios.post("/api/chat", { message: "새 사용자가 들어옴", roomName });
+    const offer = await peerConnection?.createOffer();
+    if (offer) {
+      peerConnection?.setLocalDescription(offer);
+      await axios.post("/api/chat", { offer, roomName });
+      console.log("offer 전달");
+    }
     socket?.on(roomName, (message: IMessage) => {
       console.log(message);
+    });
+
+    socket?.on(roomName + "offer", async (offer) => {
+      console.log("offer 받음");
+      peerConnection?.setRemoteDescription(offer);
+      const answer = await peerConnection?.createAnswer();
+      peerConnection?.setLocalDescription(answer);
+      await axios.post("/api/chat", { answer, roomName });
+      console.log("answer 전달");
+    });
+
+    socket?.on(roomName + "answer", async (answer) => {
+      if (!peerConnection.remoteDescription) {
+        console.log("answer 받음");
+        peerConnection?.setRemoteDescription(answer);
+      }
     });
   }
 
@@ -41,7 +93,6 @@ export default function Chat() {
         user: username,
         message: sendMessage,
       };
-
       await axios.post("/api/chat", { message, roomName });
       setSendMessage("");
     }
@@ -51,10 +102,12 @@ export default function Chat() {
     <div>
       <input value={roomName} placeholder="방이름" type={"text"} onChange={(e) => { setRoomName(e.target.value) }} />
       <button onClick={enterRoom}>방참가</button>
+      <br />
       <input value={sendMessage} placeholder="이름" type={"text"} onChange={(e) => { setSendMessage(e.target.value) }} />
       <input value={username} placeholder="텍스트" type={"text"} onChange={(e) => { setUsername(e.target.value) }} />
       <button onClick={submitSendMessage}>전송</button>
-      {/* <button onClick={joinRoom}>전송</button> */}
+
+      <video id="video" autoPlay playsInline width="400" height="400" />
     </div>
   );
 };
